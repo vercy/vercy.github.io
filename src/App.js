90c3a91cff0e8b81d0f0407f93d88e4247845713
vercy <megavercy@gmail.com>
2024-03-17 17:38:30 +0900

    consolidated content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import fblogo from './Facebook_Logo_Secondary.png'
+import ytlogo from './yt_icon_mono_dark.png'
+import ghlogo from './GitHub_Logo_White.png'
+import inlogo from './LI-In-Bug.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='circle-wrapper '>
+        <div className='tile umlaut deg-0'>
+        <a href="https://umlaut.hu">ÜD</a>
+        </div>
+        <div className='tile facebook deg-288'>
+          <a href="https://facebook.com/megavercy">
+            <img src={fblogo} alt="f" width="90px" height="90px" />
+          </a>
+        </div>
+        <div className='tile linkedin deg-216'>
+          <a href="https://www.linkedin.com/in/megavercy">
+            <img src={inlogo} alt="LinkedIn" height="70px" />
+          </a>
+        </div>
+        <div className='tile youtube deg-72'>
+          <a href="https://youtube.com/megavercy">
+            <img src={ytlogo} alt="YouTube" width="90px" height="64px" />
+          </a>
+        </div>
+        <div className='tile github deg-144'>
+          <a href="https://github.com/vercy">
+            <img src={ghlogo} alt="GitHub" width="100px" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
